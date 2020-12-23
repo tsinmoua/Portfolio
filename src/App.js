fea87104c709694de322423b11f46e7a4d337d87
Tsin Moua <tsinmoua@gmail.com>
2020-12-22 23:23:47 -0600

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Skills from './pages/Skills'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 
 function App() {
   return (
-    <>
-    TSINMOUA
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/skills' component={Skills} />
+        <Route exact path='/projects' component={Projects} />
+        <Route exact path='/contact' component={Contact} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
