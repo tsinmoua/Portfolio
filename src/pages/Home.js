e6686a060384712a304e6157d1cc9c8e7bbab9a6
Tsin Moua <tsinmoua@gmail.com>
2020-12-23 14:48:40 -0600

    add background image for home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,39 @@
 import React from "react"
+import { Grid, makeStyles, Typography } from "@material-ui/core";
+
+import Hero from '../assets/Hero.png'
+
+const useStyles = makeStyles((theme) => ({
+  divImage: {
+    backgroundImage: `url(${Hero})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%"
+  }
+}))
 
 const Home = (props) => {
+  const classes = useStyles();
+
   return (
     <div>
-      Home
+      {/* <Grid container style={{ backgroundColor: "navy" }}> */}
+      <Grid container>
+        <Grid item>
+          <div className={classes.divImage} />
+        </Grid>
+        <Grid item>
+          <Typography variant='h1'>
+            Hello,
+            <br />
+            My name is Tsin Moua
+        </Typography>
+        </Grid>
+      </Grid>
+
+
     </div>
   )
 };
