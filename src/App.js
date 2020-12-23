661aacdf4b54d610c44bef5997e52fc15f65c4ff
Tsin Moua <tsinmoua@gmail.com>
2020-12-23 11:51:25 -0600

    added tabs to navbar, render pages

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/core/styles';
 import Theme from "./components/Theme";
+import Navbar from './components/Navbar'
 
 import Home from './pages/Home'
 import About from './pages/About'
 import Skills from './pages/Skills'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
-import Navbar from './components/Navbar'
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <BrowserRouter>
         <Switch>
-          <Navbar />
+          <Navbar>
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
           <Route exact path='/skills' component={Skills} />
           <Route exact path='/projects' component={Projects} />
           <Route exact path='/contact' component={Contact} />
+          </Navbar>
         </Switch>
       </BrowserRouter>
     </ThemeProvider>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { TabList, TabContext, TabPanel } from '@material-ui/lab';
 import {
   AppBar,
   Toolbar,
-  Typography,
   IconButton,
   useScrollTrigger,
   Button,
-  Grid
+  Tab,
+  Tabs
 } from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom'
+import Home from '../pages/Home'
 
-import logo from '../assets/TsinMoua.png'
+import Logo from '../assets/TsinMoua.png'
 
 function ElevationScroll(props) {
   const { children, window } = props;
@@ -31,6 +33,16 @@ function ElevationScroll(props) {
 }
 
 const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "2rem",
+    // [theme.breakpoints.down("md")]: {
+    //   marginBottom: "2em"
+    // },
+    // [theme.breakpoints.down("xs")]: {
+    //   marginBottom: "1.25em"
+    // }
+  },
   logoContainer: {
     padding: 0,
     "&:hover": {
@@ -40,33 +52,76 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     marginLeft: 'auto'
-  }
+  },
+  tabContainer: {
+    marginLeft: 'auto',
+    marginRight: '0'
+  },
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
 }));
 
-export default function Navbar(props) {
+
+function Navbar(props) {
   const classes = useStyles();
 
+  const [value, setValue] = useState('1');
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue('1')
+    } else if (window.location.pathname === '/about' && value !== '2') {
+      setValue('2')
+    } else if (window.location.pathname === '/skills' && value !== '3') {
+      setValue('3')
+    } else if (window.location.pathname === '/projects' && value !== '4') {
+      setValue('4')
+    } else if (window.location.pathname === '/contact' && value !== '5') {
+      setValue('5')
+    }
+  }, [value])
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className={classes.root}>
-
+    <>
       <ElevationScroll {...props}>
+        <TabContext value={value}>
 
-        <AppBar position="fixed" >
-          <Toolbar disableGutters>
+          <AppBar position="fixed" >
+            <Toolbar disableGutters>
 
-            <Button disableRipple className={classes.logoContainer}>
-              <img src={logo} alt={'logo'} />
-            </Button>
+              <Button disableRipple className={classes.logoContainer} component={Link} to='/' onClick={(e) => setValue('1')}>
+                <img src={Logo} alt={'Logo'} />
+              </Button>
 
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <Tabs value={value} onChange={handleChange} className={classes.tabContainer}>
+                <Tab label="Home" value='1' component={Link} to='/' disableRipple />
+                <Tab label="About" value='2' component={Link} to='/about' disableRipple />
+                <Tab label="Skills" value='3' component={Link} to='/skills' disableRipple />
+                <Tab label="Projects" value='4' component={Link} to='/projects' disableRipple />
+                <Tab label="Contact" value='5' component={Link} to='/contact' disableRipple />
+              </Tabs>
+
+              {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
               <MenuIcon />
-            </IconButton>
+            </IconButton> */}
 
-          </Toolbar>
-        </AppBar>
-
+            </Toolbar>
+          </AppBar>
+        </TabContext>
       </ElevationScroll>
 
-    </div>
+      <div className={classes.toolbarMargin} />
+
+      {props.children}
+
+    </>
   );
 }
+
+export default Navbar
