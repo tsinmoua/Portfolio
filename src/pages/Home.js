b4693aedb4c56825f0e9837405ce8f257e756100
Tsin Moua <tsinmoua@gmail.com>
2020-12-24 17:52:16 -0600

    removed hero image, styling home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,23 @@ import hero from '../assets/Hero.png'
 const useStyles = makeStyles((theme) => ({
   divImage: {
     width: "100%",
+    height: '100%'
   },
   hello: {
     position: "relative",
     top: '-20rem',
-    left: '5rem'
+    left: '5rem',
+    [theme.breakpoints.down("md")]: {
+      fontSize: '3rem'
+    },
+  },
+  helloText: {
+    [theme.breakpoints.down("md")]: {
+      fontSize: '3rem'
+    },
+    // [theme.breakpoints.down("sm")]: {
+    //   fontSize: '3rem'
+    // }
   }
 }))
 
@@ -19,13 +31,12 @@ const Home = (props) => {
 
   return (
     <div style={{ overflowX: 'hidden' }}>
-      <img src={hero} alt='Hero img' className={classes.divImage} />
-      <img src={hero} alt='Hero img' className={classes.divImage} />
-      <img src={hero} alt='Hero img' className={classes.divImage} />
+      {/* <img src={hero} alt='Hero img' className={classes.divImage} /> */}
+      <div className={classes.divImage} />
 
       <Grid container className={classes.hello} direction='column'>
         <Grid item>
-          <Typography variant='h2' color='secondary'>
+          <Typography variant='h2' color='secondary' className={classes.helloText}>
             Hello,
            <br />
            My name is Tsin Moua
